187da3676b867aa4fbd29467ef4cdb7fd0e454a2
slaytor2005 <slaytor2005@gmail.com>
2019-10-22 20:40:36 +0300

    testing

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+//import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(" World");
+    }
+}
+
